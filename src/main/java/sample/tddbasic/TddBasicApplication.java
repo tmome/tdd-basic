8d5a1a0b2d1796843adf4ffaf9609c9c21bd5988
천현승 <woqkfehlfk@naver.com>
2023-04-22 16:50:19 +0900

    Initial commit

---
+++ src/main/java/sample/tddbasic/TddBasicApplication.java
@@ -0,0 +1,13 @@
+package sample.tddbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sample/tddbasic/TddBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.tddbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
