e181f7cbd8a4c0fca04dff8ef55e355a2a503f88
천현승 <woqkfehlfk@naver.com>
2023-04-24 18:59:25 +0900

    ApiResponseGenerator 추가 및 ErrorCode enum 추가 commit

---
+++ src/main/java/sample/tddbasic/domain/exam/support/ApiResponse.java
@@ -0,0 +1,39 @@
+package sample.tddbasic.domain.exam.support;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ApiResponse<T> {
+
+    private String code;
+
+    private String message;
+
+    private T data;
+
+    ApiResponse(ErrorCode errorCode) {
+        this(errorCode, null, null);
+    }
+
+    ApiResponse(ErrorCode errorCode, T data) {
+        this.code = errorCode.getCode();
+        this.message = errorCode.getDefaultMessage();
+        this.data = data;
+    }
+
+    ApiResponse(ErrorCode errorCode, String responseMessage, T data) {
+        this.code = errorCode.getCode();
+        this.message = (responseMessage == null ? errorCode.getDefaultMessage() : responseMessage);
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
---
+++ src/main/java/sample/tddbasic/domain/exam/support/ErrorCode.java
@@ -0,0 +1,21 @@
+package sample.tddbasic.domain.exam.support;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public enum ErrorCode {
+
+  SUCCESS("2000", "OK"),
+  UNKNOWN_ERROR("9999", "알 수 없는 오류");
+
+  private final String code;
+  private final String defaultMessage;
+
+  ErrorCode(String code, String defaultMessage) {
+    this.code = code;
+    this.defaultMessage = defaultMessage;
+  }
+
+}
